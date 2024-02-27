83d566823be4f64a6f01bd11301aa606f848fea5
Esther Park <esther514514@gmail.com>
2024-02-27 18:14:40 -0500

    website can be displayed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import Home from "./pages/Home/Home";
 import AboutUs from "./pages/AboutUs/AboutUs";
@@ -8,20 +8,21 @@ import Resources from "./pages/Resources/Resources";
 import JoinUs from "./pages/JoinUs/JoinUs";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import "./App.scss";
 
 function App() {
   return (
     <Router>
       <Header />
 
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about-us" component={AboutUs} />
-        <Route path="/roles" component={Roles} />
-        <Route path="/programs" component={Programs} />
-        <Route path="/resources" component={Resources} />
-        <Route path="/joinus" component={JoinUs} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about-us" element={<AboutUs/>} />
+        <Route path="/roles" element={<Roles/>} />
+        <Route path="/programs" element={<Programs/>} />
+        <Route path="/resources" element={<Resources/>} />
+        <Route path="/joinus" element={<JoinUs/>} />
+      </Routes>
 
       <Footer />
     </Router>
