d3373d6b3fa6e11412bf46b597f6ccdf613443da
Esther Park <esther514514@gmail.com>
2024-03-03 00:41:48 -0500

    minor details fixed

--- frontend/src/components/HomeSections/HomeSection1/HomeSection1.jsx
+++ frontend/src/components/HomeSections/HomeSection1/HomeSection1.jsx
@@ -3,6 +3,9 @@ import { Button } from "../../Button/Button";
 import "./HomeSection1.scss";
 
 export const HomeSection1 = ({ title, subtitle, img }) => {
+  const handleLinkClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   return (
     <div className="home-section1">
       <div className="home-section1-content">
@@ -15,7 +18,11 @@ export const HomeSection1 = ({ title, subtitle, img }) => {
             className="home-section1-subtitle"
             dangerouslySetInnerHTML={{ __html: subtitle }}
           />
-          <Button name="Get Involved" />
+          <Button
+            name="Get Involved"
+            link="/joinus"
+            onClick={handleLinkClick}
+          />
         </div>
       </div>
       <img className="home-section1-picture" alt="Section picture" src={img} />
--- frontend/src/components/HomeSections/HomeSection2/HomeSection2.jsx
+++ frontend/src/components/HomeSections/HomeSection2/HomeSection2.jsx
@@ -3,6 +3,9 @@ import { Button } from "../../Button/Button";
 import "./HomeSection2.scss";
 
 export const HomeSection2 = ({ title, subtitle, img }) => {
+  const handleLinkClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   return (
     <div className="home-section2">
       <img
@@ -20,7 +23,11 @@ export const HomeSection2 = ({ title, subtitle, img }) => {
             className="home-section2-subtitle"
             dangerouslySetInnerHTML={{ __html: subtitle }}
           />
-          <Button name="Our Mission" />
+          <Button
+            name="Our Mission"
+            link="/about-us"
+            onClick={handleLinkClick}
+          />
         </div>
       </div>
     </div>
