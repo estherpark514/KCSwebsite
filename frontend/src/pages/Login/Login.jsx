1b8ff53518c0729c4f05da149825737b07ef343f
Esther Park <esther514514@gmail.com>
2024-03-29 07:00:44 -0400

    signup frontend done

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,29 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../../components/Button/Button";
 import "./Login.scss";
 
-function Login () {
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div className="box">
-      <div className="group">
-        <div className="text-wrapper">Log In</div>
-        <div className="div">
-          <div className="overlap-group">
-            <div className="text-wrapper-2">Your email address</div>
-          </div>
-          <div className="overlap">
-            <div className="text-wrapper-2">Password</div>
-          </div>
-          <Button
-            name="Forgot your password?"
-            className="home-section-1-button"
-            text="Forgot your password?"
+    <div className="group">
+      <div className="title">Log In</div>
+      <div className="login-content">
+        <div className="text-wrapper-1">Email</div>
+        <input
+          type="email"
+          className="text-wrapper-2"
+          placeholder="Your email address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <div className="text-wrapper-1">Password</div>
+        <div className="overlap">
+          <input
+            type="password"
+            className="text-wrapper-2"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <div className="text-wrapper-3">Email</div>
-          <div className="text-wrapper-4">Password</div>
-          <p className="p">*Your email address is your georgia tech account</p>
         </div>
       </div>
+      <Button name="Log In" className="gray" link="/" style={{ marginBottom: '20px' }} />
+      <div className="text-wrapper-4">Forgot your password?</div>
     </div>
   );
 }
--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -1,29 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../../components/Button/Button";
 import "./Signup.scss";
 
-function Signup () {
+function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [buttonText, setButtonText] = useState("Verify");
+
+  const handleButtonClick = () => {
+    setButtonText("Resend");
+  };
+
   return (
-    <div className="box">
-      <div className="group">
-        <div className="text-wrapper">Log In</div>
-        <div className="div">
-          <div className="overlap-group">
-            <div className="text-wrapper-2">Your email address</div>
+    <div className="group">
+      <div className="title">Sign Up</div>
+      <div className="text-wrapper-3">
+        Only students with a Georgia Tech email account are eligible to create
+        an account.
+      </div>
+      <div className="signup-content">
+        <div className="signup-section">
+          <div className="text-wrapper-1">Email</div>
+          <div className="container">
+            <input
+              type="email"
+              className="text-wrapper-2"
+              placeholder="Your email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Button
+              name={buttonText}
+              className="white"
+              link="#"
+              onClick={handleButtonClick}
+            />
           </div>
-          <div className="overlap">
-            <div className="text-wrapper-2">Password</div>
+          <div className="text-wrapper-3">
+            *Your email address must be Georgia Tech email address
           </div>
-          <Button
-            name="Forgot your password?"
-            className="home-section-1-button"
-            text="Forgot your password?"
-          />
-          <div className="text-wrapper-3">Email</div>
-          <div className="text-wrapper-4">Password</div>
-          <p className="p">*Your email address is your georgia tech account</p>
+        </div>
+        <div className="signup-section">
+          <div className="container">
+            <input
+              type="verification"
+              className="text-wrapper-2"
+              placeholder="Your verification code"
+            />
+            <Button name="verify" className="white" link="/" />
+          </div>
+        </div>
+        <div className="signup-section">
+          <div className="text-wrapper-1">Password</div>
+          <div className="container">
+            <input
+              type="password"
+              className="text-wrapper-2"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="text-wrapper-3">
+            *Password must be at least 8 characters long
+          </div>
+        </div>
+        <div className="signup-section">
+          <div className="text-wrapper-1">First Name (ENG)</div>
+          <div className="container">
+            <input
+              type="first name (eng)"
+              className="text-wrapper-2"
+              placeholder="First name"
+            />
+          </div>
+        </div>
+        <div className="signup-section">
+          <div className="text-wrapper-1">Last Name (ENG)</div>
+          <div className="container">
+            <input
+              type="last name (eng)"
+              className="text-wrapper-2"
+              placeholder="Last name"
+            />
+          </div>
+        </div>
+        <div className="signup-section">
+          <div className="text-wrapper-1">Major</div>
+          {/* <Dropdown options={options} /> */}
+        </div>
+        <div className="signup-section">
+          <div className="text-wrapper-1">Class Standing</div>
+          {/* <Dropdown options={options} /> */}
         </div>
       </div>
+      <Button name="Create an account" className="white" link="/" />
     </div>
   );
 }
