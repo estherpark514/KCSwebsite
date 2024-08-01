412a54a50caf9e71381650881fd4b7f4f69cf25f
Esther Park <esther514514@gmail.com>
2024-07-31 20:46:19 -0400

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -23,6 +23,10 @@ DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['*'])
 
+CSRF_TRUSTED_ORIGINS = ["kcswebsite-production.up.railway.app/"]
+
+CSRF_ALLOWED_ORIGINS = ["kcswebsite-production.up.railway.app/"]
+
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Application definition
